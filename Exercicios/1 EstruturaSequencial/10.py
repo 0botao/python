7b14c6366d8ff06deaae731890c563cab4839bba
Your Namedavid <contatodavidbotao@gmail.com>
2024-09-04 12:30:44 -0300

    ...

--- Exercicios/1 EstruturaSequencial/10.py
+++ Exercicios/1 EstruturaSequencial/10.py
@@ -1,2 +1,5 @@
 # Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit. 
 
+c  = float(input(f" digite a temperatura em Celsius "))
+
+print(f" a temperatura em graus Celsius é de exatamente {((c * 1.7) + 32):.0f}")
