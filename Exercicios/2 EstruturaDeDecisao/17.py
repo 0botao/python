f433ae329ce4aefd928d43e36b2cbcfe8a04640d
david botao <contatodavidbotao@gmail.com>
2024-10-22 19:56:30 -0300

    .

--- Exercicios/2 EstruturaDeDecisao/17.py
+++ Exercicios/2 EstruturaDeDecisao/17.py
@@ -1,11 +1,15 @@
 # Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesma é uma data válida. 
 
-def verificadorVazio(a):
+def verificadorVazio(a, limite):
     while True:
         a = a.strip()
 
         if a == "":
             print(' nao pode deixar o espaço em branco ')
+            return None
+        
+        if len(a) >  limite:
+            print(f' o limite de caracters é de {limite} !!')
             return None
         try:
             a = float(a)
@@ -14,10 +18,23 @@ def verificadorVazio(a):
             print('erro de caracteres ')
             return None 
         
+def fomatar(a,b,c):
+    data = print(f'{dia}/{mes}/{ano}')
+    return data
+
 while True:
     dia = input(' digite o dia \n')
-    dia = verificadorVazio(dia) 
+    dia = verificadorVazio(dia, 2) 
     mes = input(' digite o mes \n') 
+    mes = verificadorVazio(mes,2 )
+    ano = input(' digite o ano \n')
+    ano = verificadorVazio(ano,4) 
+
+    fomatar(dia,mes,ano)
+
+
+
+
 
 
 
